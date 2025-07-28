72af8646817f640ad4b5fd21cf7e0cab1ed0c1f8
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-07-28 09:31:15 -0400

    NetFPGA added and tested with uart echo

---
+++ NetFPGA-1G-CML/system_constr.tcl
@@ -0,0 +1,54 @@
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
+
+## I/O delay constraints
+create_clock -period 20.000 -name VIRTUAL_clk_out1_system_clk_wiz_1 -waveform {0.000 10.000}
+
+set_input_delay -clock VIRTUAL_clk_out1_system_clk_wiz_1 -max 50.000 [get_ports ftdi_tx]
+set_input_delay -clock VIRTUAL_clk_out1_system_clk_wiz_1 -min 20.000 [get_ports ftdi_tx]
+set_output_delay -clock VIRTUAL_clk_out1_system_clk_wiz_1 -max 50.000 [get_ports ftdi_rx]
+set_output_delay -clock VIRTUAL_clk_out1_system_clk_wiz_1 -min 20.000 [get_ports ftdi_rx]
+set_input_delay -clock VIRTUAL_clk_out1_system_clk_wiz_1 -max 100.000 [get_ports resetn]
+set_input_delay -clock VIRTUAL_clk_out1_system_clk_wiz_1 -min 50.000 [get_ports resetn]
+
+## it doesn't actually matter
+set_false_path -through [get_ports ftdi_tx]
+set_false_path -through [get_ports ftdi_rx]
+set_false_path -through [get_ports resetn]
+
+# ##Pmod Headers
+# ##Pmod Header pmod_jb jtag 4, 8, 9, 10
+# if {[get_ports -quiet tck] ne "" } {
+#   create_clock -period 10.000 -name jtag_tck -waveform {0.000 5.000} [get_ports tck]
+# 
+#   set_property PACKAGE_PIN H14 [get_ports tms]
+#   set_property PULLUP true [get_ports tms]
+#   set_property IOSTANDARD LVCMOS33 [get_ports tms]
+# 
+#   set_property PACKAGE_PIN F13 [get_ports tdo]
+#   set_property PULLUP true [get_ports tdo]
+#   set_property IOSTANDARD LVCMOS33 [get_ports tdo]
+# 
+#   set_property PACKAGE_PIN G13 [get_ports tdi]
+#   set_property PULLUP true [get_ports tdi]
+#   set_property IOSTANDARD LVCMOS33 [get_ports tdi]
+# 
+#   set_property PACKAGE_PIN H16 [get_ports tck]
+#   set_property PULLDOWN true [get_ports tck]
+#   set_property IOSTANDARD LVCMOS33 [get_ports tck]
+# }
+ 
+##Micro SD Connector
+set_property -dict { PACKAGE_PIN AA18     IOSTANDARD LVCMOS18 } [get_ports { sd_spi_sclk }]; #IO_L9P_T1_DQS_AD7P_35 Sch=sd_sck
+set_property -dict { PACKAGE_PIN AF18     IOSTANDARD LVCMOS18 } [get_ports { sd_spi_mosi }]; #IO_L16N_T2_35 Sch=sd_cmd
+set_property -dict { PACKAGE_PIN AE17     IOSTANDARD LVCMOS18 } [get_ports { sd_spi_miso }]; #IO_L16P_T2_35 Sch=sd_dat[0]
+set_property -dict { PACKAGE_PIN AE18     IOSTANDARD LVCMOS18 } [get_ports { sd_spi_csn }]; #IO_L14N_T2_SRCC_35 Sch=sd_dat[3]
+
+#pmodUART
+set_property -dict { PACKAGE_PIN F20     IOSTANDARD LVCMOS33 } [get_ports { ftdi_rts }]; #IO_L9P_T1_DQS_AD7P_35 Sch=sd_sck
+set_property -dict { PACKAGE_PIN E15     IOSTANDARD LVCMOS33 } [get_ports { ftdi_rx  }]; #IO_L16N_T2_35 Sch=sd_cmd
+set_property -dict { PACKAGE_PIN H18     IOSTANDARD LVCMOS33 } [get_ports { ftdi_tx  }]; #IO_L16P_T2_35 Sch=sd_dat[0]
+set_property -dict { PACKAGE_PIN G19     IOSTANDARD LVCMOS33 } [get_ports { ftdi_cts }]; #IO_L14N_T2_SRCC_35 Sch=sd_dat[3]
+
+set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]
+
+
